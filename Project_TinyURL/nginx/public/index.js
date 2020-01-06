43cf8ec4f6b9990be07be8f3bd72d90631b3c511
wwwumr <1127858301@qq.com>
2020-01-06 09:01:33 +0800

    添加固定ip及错误处理

--- Project_TinyURL/nginx/public/index.js
+++ Project_TinyURL/nginx/public/index.js
@@ -3,17 +3,48 @@ let URLTYPE = {
     long: 1, //生成短链接
 };
 let URLTEXT = ["请输入短链接,按回车确认", "请输入长链接，按回车确认",];
+let backendUrl = "http://111.229.216.12:9001/";
+let interfaces = {
+    build: backendUrl + "set",
+    look: backendUrl + "get",
+}
 
 //设置服务方式
 let urlType = URLTYPE.tiny;
 document.getElementById("urlTextId").innerHTML = URLTEXT[urlType];
 
 function buildTiny(longUrl) {
-    document.getElementById("outputId").innerHTML = longUrl;
+    let result = longUrl;
+    axios
+        .get(interfaces.build + "?id=" + longUrl)
+        .then((res) => {
+            result = res.data;
+            document.getElementById("outputId").innerHTML = result;
+        })
+        .catch((err) => {
+            if (err.response) {
+                document.getElementById("outputId").innerHTML = "error: " + err.response.data;
+            } else {
+                document.getElementById("outputId").innerHTML = "error: " + err.message;
+            }
+        })
 }
 
 function lookForUrl(tiny) {
-    document.getElementById("outputId").innerHTML = tiny;
+    let result = tiny;
+    axios
+        .get(interfaces.look + "?id=" + tiny)
+        .then((res) => {
+            result = res.data;
+            document.getElementById("outputId").innerHTML = result;
+        })
+        .catch((err) => {
+            if (err.response) {
+                document.getElementById("outputId").innerHTML = "error: " + err.response.data;
+            } else {
+                document.getElementById("outputId").innerHTML = "error: " + err.message;
+            }
+        })
 }
 
 function listenInput() {
