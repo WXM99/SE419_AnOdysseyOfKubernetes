6991b02a55fc015f6738b595ee49008b6a426d12
Wonderful23 <2596409301@qq.com>
2020-01-07 09:55:59 +0800

    tinyUrl backup

---
+++ Project_TinyURL/rr/src/main/java/com/example/demo/Entity/Link.java
@@ -0,0 +1,22 @@
+package com.example.demo.Entity;
+
+public class Link {
+    private String shortUrl;
+    private String longUrl;
+
+    public String getShortUrl() {
+        return shortUrl;
+    }
+
+    public void setShortUrl(String shortUrl) {
+        this.shortUrl = shortUrl;
+    }
+
+    public String getLongUrl() {
+        return longUrl;
+    }
+
+    public void setLongUrl(String longUrl) {
+        this.longUrl = longUrl;
+    }
+}
